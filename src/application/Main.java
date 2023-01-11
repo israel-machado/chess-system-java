d246e1d0509dcbd405621810956ca8ab11a24929
Israel Machado <israelmach26@gmail.com>
2023-01-11 11:34:44 -0300

    First Class: Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("hello");
+        Position pos = new Position(3, 5);
+
+        System.out.print(pos);
     }
 }
