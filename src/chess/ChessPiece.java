b966595fa43f75b58f8015c6ba06a5027d769e15
Israel Machado <israelmach26@gmail.com>
2023-01-12 13:59:26 -0300

    BoardException and defensive programming

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardgame.Piece;
 public class ChessPiece extends Piece {
 
     private final Color color;
+    private Integer moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
